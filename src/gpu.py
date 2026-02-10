3b5b5715a23f2c4259db751ce1f061342852f99c
Daniel <dgarridoalex@gmail.com>
2026-02-09 17:25:50 -0800

    initial gpu.py

---
+++ src/gpu.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+import subprocess
+import json
+
+ROCM_CMD = "rocm-smi --showmeminfo vram --showuse --showtemp --json".split()
+
+@dataclass
+class GPUInfo:
+    name: str
+    vram_total_mb: int
+    vram_used_mb: int
+    vram_free_mb: int
+    util: int
+    temp: int
+
+def init_gpu():
+    global gpu_info
+    data = get_gpu_stats()
+    gpu_info = GPUInfo('RX 9070XT',data['vram_free'],
+    data['vram_used'], data['vram_free'], data['util'],
+    data['temp_c']
+    )
+    return
+
+def get_gpu_stats():
+    result = subprocess.run(ROCM_CMD, capture_output=True, text=True, check=True)
+    output = json.loads(result.stdout)
+    return rocm_smi_parser(output)
+
+    
+def rocm_smi_parser(output: dict) -> dict:
+    stats = {}
+    for field in output['card0']:
+        match field:
+            case 'VRAM Total Memory (B)':
+                stats['vram_total'] = bytes_to_mb(int(output['card0'][field]))
+            case  'VRAM Total Used Memory (B)':
+                stats['vram_used'] = bytes_to_mb(int(output['card0'][field]))
+            case 'Temperature (Sensor edge) (C)':
+                stats['temp_c'] = output['card0'][field]
+            case 'GPU use (%)':
+                stats['util'] = output['card0'][field]
+    stats['vram_free'] = stats['vram_total'] - stats['vram_used']
+    return stats
+
+def bytes_to_mb(bytes: int):
+    return bytes // (1024 * 1024)
+
+init_gpu()
+print(gpu_info)
